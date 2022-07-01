a065f428113f7b82320855ef6231c4170bdeaba0
Jonathan Bidwell <jbidwellb@gmail.com>
2022-07-01 19:25:04 -0400

    Avances de la tercera parte y modificaciones de los ej previos

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,5 +1,5 @@
 
-
+// Francisco aquí te pido que me ayudes... Intenté definir variables para cada getElementById pero al hacerlo, y luego definir sum = input1 + input2 + input3, el código no reconoce los valores ingresados y la suma queda igual a cero...
 function verify() {
     if (
         document.getElementById("input1").valueAsNumber + document.getElementById("input2").valueAsNumber + document.getElementById("input3").valueAsNumber <= 10
---
+++ assets/js/script3.js
@@ -0,0 +1,2 @@
+let input1 = document.getElementById("input1").valueAsNumber;
+console.log(input1);
